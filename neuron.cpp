3cc46c3eb1ff05f1084af5f4b9c755520947c688
Your Name <you@example.com>
2020-05-09 22:17:58 -0300

    chenge objects to use class and methods with inputs

--- neuron.cpp
+++ neuron.cpp
@@ -10,13 +10,14 @@ using std::endl;
 
 int main() {
 
-    utils rede;
-    rede.neuron();
+    /*Train de network*/
+    utils().neuron();
 
-    rede.sigma_output(0, 0);
-    rede.sigma_output(0, 1);
-    rede.sigma_output(1, 0);
-    rede.sigma_output(1, 1);
+    /*here we can test passing the inputs*/
+    utils().sigma_output(0, 0);
+    utils().sigma_output(0, 1);
+    utils().sigma_output(1, 0);
+    utils().sigma_output(1, 1);
     
     return 0;
 }
